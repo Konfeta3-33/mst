198b0ef680c686dee856b5e6e709544284c5c050
Valentina Kunina <konfeta333@gmail.com>
2025-03-12 16:53:07 +0800

    Add styles for media queries: 1024; translated on English

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector(".header");
 
     window.addEventListener("scroll", function () {
-        console.log("hi!")
-        console.log(document.querySelector('.header').classList)
         if (window.scrollY > 50) {
             header.classList.add("scrolled");
         } else {
@@ -11,16 +9,3 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
-//
-// document.addEventListener("DOMContentLoaded", () => {
-//     const features = document.querySelectorAll(".feature-item");
-//     const observer = new IntersectionObserver((entries) => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add("fade-in");
-//             }
-//         });
-//     }, { threshold: 0.5 });
-//
-//     features.forEach(item => observer.observe(item));
-// });
